abf8e2b0867a248500959468e94e4d7589cb589b
tienftang <61549274+tienftang@users.noreply.github.com>
2020-03-26 01:30:54 +0000

    Updated SlidingWindows authors
    This currently only calculates MAF estimates.

--- Analyser/src/bsa_analyser/github/io/SlidingWindows.java
+++ Analyser/src/bsa_analyser/github/io/SlidingWindows.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bsa_18032020;
 
 import com.sun.javafx.property.adapter.PropertyDescriptor;
@@ -16,7 +11,7 @@ import java.util.Set;
 
 /**
  *
- * @author tee_t
+ * @author Tien Tang, Kathryn Kananen
  */
 public class SlidingWindows {
 
